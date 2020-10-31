4401e267db4301af5a2ca5b22b30145d3da3cafb
HYBRIDMYSTERIO <73470518+HYBRIDMYSTERIO@users.noreply.github.com>
2020-10-31 19:04:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,40 @@
-var fixedRectangle, movingRectangle;
+var car,wall;
 
-
+var speed,weight;
 
 
 function setup() {
-  createCanvas(800,400);
-  fixedRectangle = createSprite(400, 200, 50, 50);
-  fixedRectangle.shapeColor = "green";
-  fixedRectangle.debug = true;
-  movingRectangle = createSprite(200,100,50,50);
-  movingRectangle.shapeColor = "green";
-  movingRectangle.debug = true;
+  createCanvas(1600,400);
+  car = createSprite(50, 200, 50, 50);
+  wall = createSprite(1500,200,60,height/2);
+  wall.shapeColor = "80,80,80";
+
+  speed = random(55,90);
+  weight = random(400,1500);
 }
 
 function draw() {
-  background(255,255,255);
-  movingRectangle.x = World.mouseX;
-  movingRectangle.y = World.mouseY;
-  
-  if(movingRectangle.x - fixedRectangle.x < movingRectangle.width/2 + fixedRectangle.width/2 &&
-    fixedRectangle.x - movingRectangle.x < movingRectangle.width/2 + fixedRectangle.width/2 &&
-    movingRectangle.y - fixedRectangle.y < movingRectangle.height/2 + fixedRectangle.height/2 &&
-    fixedRectangle.y - movingRectangle.y < movingRectangle.height/2 + fixedRectangle.height/2){
-      movingRectangle.shapeColor = "red";
-      fixedRectangle.shapeColor = "red";
+  background("black");  
+  car.velocityX = speed;
+
+  if(wall.x - car.x < (car.width + wall.width)/2){
+    car.velocityX = 0;
+
+    var deformation = 0.5 * weight * speed * speed/22509;
+
+    if(deformation < 100)
+    {
+      car.shapeColor = "green";
     }
-    else
-  {
-    movingRectangle.shapeColor = "green";
-    fixedRectangle.shapeColor = "green";
+    if(deformation < 180 && deformation > 100)
+    {
+      car.shapeColor = "yellow";
+    }
+    if(deformation > 180)
+    {
+      car.shapeColor = "red";
+    }
   }
 
-  
   drawSprites();
 }
-
